ee7358213d3bbf44870bc03d679d8f4437ca1be5
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-09-16 21:38:24 -0300

    remove unnecessary comments for AppDelegate/SceneDelegate methods

--- StreamingApp/StreamingApp/SceneDelegate.swift
+++ StreamingApp/StreamingApp/SceneDelegate.swift
@@ -2,14 +2,17 @@ import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        configureWindow(using: windowScene)
+    }
+}
+
+extension SceneDelegate {
+    func configureWindow(using windowScene: UIWindowScene) {
         let window = UIWindow(windowScene: windowScene)
-        let rootVC = ViewController()
-        window.rootViewController = rootVC
+        window.rootViewController = ViewController()
         self.window = window
         window.makeKeyAndVisible()
     }
 }
-
