eb2f4787bb93115f281cd065c0d00854e199f078
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-09-16 21:42:05 -0300

    add logic to use .xcconfig files

--- StreamingApp/StreamingApp/ViewController.swift
+++ StreamingApp/StreamingApp/ViewController.swift
@@ -4,5 +4,10 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        printAPIKey()
+    }
+
+    func printAPIKey() {
+        print("\(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "")")
     }
 }
